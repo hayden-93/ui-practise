8cda7ad19c7b3051db4704eaf373bfa4257b4409
Hayden Buckley-Smith <hayden@abstack.co>
2022-07-26 16:56:55 +0100

    chore: update config files
    add custom theme in tailwind config and add experimental image component in next config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Montserrat"],
+      body: ["Fraunces"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          "dark-cyan": "hsl(158, 36%, 37%)",
+          cream: "hsl(30, 38%, 92%)",
+        },
+        neutral: {
+          "dark-blue": "hsl(212, 21%, 14%)",
+          "dark-grayish-blue": "hsl(228, 12%, 48%)",
+          white: "hsl(0, 0%, 100%)",
+        },
+      },
+      screens: {
+        mobile: "375px",
+      },
+    },
   },
+
   plugins: [],
 };
