253eb25260b60b29c02c44d94a3626605785c4fb
Hayden Buckley-Smith <hayden@abstack.co>
2022-07-27 15:13:30 +0100

    feat: nft preview card project
    create nft preview card page and add fonts to Head

---
+++ pages/nft-preview-card.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import Head from "next/head";
+
+import { Image, Link } from "../src/components";
+
+const NFTPreviewCard = () => {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div></div>
+    </>
+  );
+};
+
+export default NFTPreviewCard;
