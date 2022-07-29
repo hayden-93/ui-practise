f427f916a55090fc058a5b0b7050b1949b4cc58d
Hayden Buckley-Smith <hayden@abstack.co>
2022-07-29 14:05:46 +0100

    feat: layout components
    add application-level Layout with Header/Footer components

---
+++ src/layout/Header.tsx
@@ -0,0 +1,31 @@
+import { Link } from "../components";
+
+const links = [
+  { href: "#linkedin", label: "LinkedIn" },
+  { href: "#github", label: "GitHub" },
+];
+
+export const Header = () => {
+  return (
+    <header className="max-w-screen-max mx-auto py-6">
+      <nav className="flex items-center justify-between text-neutral-600">
+        <Link
+          href="/"
+          className="text-2xl font-semibold hover:text-neutral-900"
+        >
+          HBS
+        </Link>
+        <div className="flex space-x-6 text-base font-normal">
+          {links.map(({ href, label }) => (
+            <span
+              key={href}
+              className="cursor-pointer decoration-2 decoration-neutral-900 underline-offset-4 hover:underline hover:text-neutral-900"
+            >
+              {label}
+            </span>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from "react";
+
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
